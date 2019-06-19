d92a53d7c222b8810afaba4205997c5eddf0dac8
phooning <davidpham@utexas.edu>
2019-06-18 23:08:07 -0500

    Alpha sign up, ScrollMagic setup, controller for trigger, and base animation

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+const { TimelineMax } = window;
+const controller = new ScrollMagic.Controller();
+
+$('.project').each(function(){
+  const imgOverlay = $(this).find('.overlay');
+  const projectInfo = $(this).find('.project-info');
+  const smallTitle = $(this).find('.small-title');
+  const projectLink = $(this).find('.project-link');
+  const title = $(this).find('h4');
+  let animateIn = new TimelineMax();
+
+  animateIn
+    .fromTo(
+      imgOverlay,
+      2,
+      {skewX: 30, scale: 1.5},
+      {
+        skewX: 0,
+        xPercent: 100,
+        transformOrigin: '0% 100%',
+        ease: Power4.easeOut
+      }
+    )
+    .from(
+      projectInfo,
+      1,
+      {
+        scaleY: 0,
+        transformOrigin: 'bottom-left'
+      },
+      '-=1.5'
+    )
+    .from(
+      smallTitle,
+      0.3,
+      {autoAlpha: 0, y: 30, ease: Power4.easeOut},
+      '-=0.8'
+    )
+    .from(
+      projectLink, 0.3,
+      {autoAlpha: 0, y: 30, ease: Power4.easeOut},
+      '-=0.8'
+    )
+    .from(
+      title,
+      0.3,
+      {autoAlpha: 0, y: 30, ease: Power4.easeOut},
+      '-=0.8'
+    );
+
+  const scene = new ScrollMagic.Scene({
+  triggerElement: this
+  }).addIndicators().setTween(animateIn).addTo(controller);
+});
+
+
